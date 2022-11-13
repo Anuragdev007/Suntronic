1881ca3120c79436a6c7c1643ae0602f250ec205
Anurag <anuragbhardwaj1333@gmail.com>
2022-11-13 13:47:05 +0530

    new commit

--- script.js
+++ script.js
@@ -118,3 +118,9 @@ $('[data-fancybox="gallery"]').fancybox({
     loop: false,
     protect: true
 });
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
